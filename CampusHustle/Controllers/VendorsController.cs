ea70d06c7e60fefa07394414da0ef46c2e0eef52
Egahi <okworisamuel6@gmail.com>
2019-06-10 03:07:30 +0100

    Implemented CRUD api

--- CampusHustle/Controllers/VendorsController.cs
+++ CampusHustle/Controllers/VendorsController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
-using ;
 using VendorService;
 
 namespace CampusHustle.Controllers
@@ -28,6 +28,62 @@ namespace CampusHustle.Controllers
                 yield return vendor;
             }
         }
- 
+        public HttpResponseMessage Post(Vendor vendor)
+        {
+            using (CampusHustleEntities context = new CampusHustleEntities())
+            {
+                context.Vendors.Add(vendor);
+                context.SaveChanges();
+                return Request.CreateResponse(HttpStatusCode.Created, vendor);
+            }
+        }
+
+        public HttpResponseMessage Put(long id, Vendor vendor)
+        {
+            using (CampusHustleEntities context = new CampusHustleEntities())
+            {
+                Vendor existingVendor = context.Vendors.Find(id);
+
+                if (existingVendor != null)
+                {
+                    existingVendor.FirstName = vendor.FirstName;
+                    existingVendor.LastName = vendor.LastName;
+                    existingVendor.PhoneNumber = vendor.PhoneNumber;
+                    existingVendor.Email = vendor.Email;
+                    existingVendor.Campus = vendor.Campus;
+                    existingVendor.Service = vendor.Service;
+                    existingVendor.Address = vendor.Address;
+
+                    context.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, vendor);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, vendor);
+                }
+                
+
+                
+            }
+        }
+
+        public HttpResponseMessage Delete(long id)
+        {
+            using (CampusHustleEntities context = new CampusHustleEntities())
+            {
+                Vendor vendor = context.Vendors.Find(id);
+
+                if (vendor != null)
+                {
+                    context.Vendors.Remove(vendor);
+                    context.SaveChanges();
+                    return Request.CreateResponse(HttpStatusCode.OK, vendor);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, vendor);
+                }
+            }
+        }
     }
 }
