9bf1df88eefc0dbcbbd6a7225895d872c31147e7
Egahi <okworisamuel6@gmail.com>
2019-06-10 02:35:14 +0100

    Initial commit

---
+++ CampusHustle/Controllers/VendorsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ;
+using VendorService;
+
+namespace CampusHustle.Controllers
+{
+    public class VendorsController : ApiController
+    {
+        public IEnumerable<Vendor> Get()
+        {
+            using (CampusHustleEntities context = new CampusHustleEntities())
+            {
+                return context.Vendors.ToList();
+            }
+        }
+
+        public IEnumerable<Vendor> Get(long id)
+        {
+            using (CampusHustleEntities context = new CampusHustleEntities())
+            {
+                var vendor = context.Vendors.Find(id);
+
+                yield return vendor;
+            }
+        }
+ 
+    }
+}
