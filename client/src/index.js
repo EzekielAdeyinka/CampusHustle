ada046fb9ebed4c2192de66ef88247c6100bbc68
timzprof <timilehin.olumofin@gmail.com>
2019-07-05 15:55:32 +0100

    Setup Redux for State Management

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import authReducer from "./store/reducers/auth";
+import vendorsReducer from "./store/reducers/vendors";
+
+const composeEnhancers =
+	process.env.NODE_ENV === "development"
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
+		  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+				trace: true,
+				traceLimit: 25
+		  })
+		: null || compose;
+
+const rootReducer = combineReducers({
+	auth: authReducer,
+	vendors: vendorsReducer
+});
+
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk, logger))
+);
 
 const app = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
