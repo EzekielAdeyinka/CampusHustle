3572a1274262c2e2b9f90f517c4cc0b7fb5717b5
timzprof <timilehin.olumofin@gmail.com>
2019-07-05 15:11:14 +0100

    Added All Vendors Page and changed App Title

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./containers/Home/Home";
-import Search from './containers/Search/Search';
+import Search from "./containers/Search/Search";
+import AllVendors from "./containers/AllVendors/AllVendors";
 
 class App extends Component {
 	render() {
@@ -9,6 +10,7 @@ class App extends Component {
 			<Switch>
 				<Route path="/" exact component={Home} />
 				<Route path="/search" component={Search} />
+				<Route path="/all-vendors" component={AllVendors} />
 			</Switch>
 		);
 	}
---
+++ client/src/containers/AllVendors/AllVendors.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Footer from "../../components/Footer/Footer";
+import Overlay from "../../components/UI/Overlay/Overlay";
+import Header from "../../components/Header/Header";
+import Banner from "../../components/Banner/Banner";
+import VendorCard from "../../components/VendorCard/VendorCard";
+
+class AllVendors extends React.Component {
+	state = {
+		vendors: []
+	};
+	render() {
+		let results = <p>No vendors found</p>;
+		if (this.state.vendors.length > 0) {
+			results = this.state.vendors.map(vendor => (
+				<VendorCard vendor={vendor} />
+			));
+		}
+		return (
+			<React.Fragment>
+				<Overlay />
+				<Header />
+				<main>
+					<section class="Search_banner">
+						<h3>All Vendors</h3>
+					</section>
+					<section class="Page_section">
+						<div class="Container">
+							<div class="Card_list Vendors_list">{results}</div>
+						</div>
+					</section>
+					<Banner />
+				</main>
+				<Footer />
+			</React.Fragment>
+		);
+	}
+}
+
+export default AllVendors;
