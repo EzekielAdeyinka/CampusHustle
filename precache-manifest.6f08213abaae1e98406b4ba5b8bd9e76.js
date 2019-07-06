44ee819a759b22568946d1eaaa80ed45ccc96b63
timzprof <timilehin.olumofin@gmail.com>
2019-07-06 19:35:19 +0100

    Pushing Prod files to Staging branch

---
+++ precache-manifest.6f08213abaae1e98406b4ba5b8bd9e76.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f58c4c5ed08b239e0264efb02ff5fe4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd01f1083678acec8773",
+    "url": "/static/css/3.9a905d3c.chunk.css"
+  },
+  {
+    "revision": "42d44194ced39986549b",
+    "url": "/static/css/4.9a905d3c.chunk.css"
+  },
+  {
+    "revision": "5d3ac52946bf0be3210e",
+    "url": "/static/css/5.9a905d3c.chunk.css"
+  },
+  {
+    "revision": "9262ff40d4c9d1e50171",
+    "url": "/static/css/6.0ff60f11.chunk.css"
+  },
+  {
+    "revision": "4ffb1341fb5962f25aff",
+    "url": "/static/css/7.4a3b87f7.chunk.css"
+  },
+  {
+    "revision": "af76910d48c344540d13",
+    "url": "/static/css/main.ed97235b.chunk.css"
+  },
+  {
+    "revision": "90ddca1c196e322c4949",
+    "url": "/static/js/2.21c3c924.chunk.js"
+  },
+  {
+    "revision": "fd01f1083678acec8773",
+    "url": "/static/js/3.f55bc63e.chunk.js"
+  },
+  {
+    "revision": "42d44194ced39986549b",
+    "url": "/static/js/4.01a9fce5.chunk.js"
+  },
+  {
+    "revision": "5d3ac52946bf0be3210e",
+    "url": "/static/js/5.e5fd7db5.chunk.js"
+  },
+  {
+    "revision": "9262ff40d4c9d1e50171",
+    "url": "/static/js/6.d883049c.chunk.js"
+  },
+  {
+    "revision": "4ffb1341fb5962f25aff",
+    "url": "/static/js/7.a80aee25.chunk.js"
+  },
+  {
+    "revision": "e2cfaa4a2b6008e9fb55",
+    "url": "/static/js/8.4ea026e0.chunk.js"
+  },
+  {
+    "revision": "24a95fbde418222889f2",
+    "url": "/static/js/9.3e17bffc.chunk.js"
+  },
+  {
+    "revision": "af76910d48c344540d13",
+    "url": "/static/js/main.9b2003df.chunk.js"
+  },
+  {
+    "revision": "aa4d59c0356f151e0683",
+    "url": "/static/js/runtime~main.629292be.js"
+  },
+  {
+    "revision": "025b1add25ae7f5da511acbb0662a74e",
+    "url": "/static/media/artwork.025b1add.jpg"
+  },
+  {
+    "revision": "60e65e69ab065fb7ff0a7f58d46f7ab1",
+    "url": "/static/media/background.60e65e69.jpg"
+  },
+  {
+    "revision": "b92b0dab28b3ce3d94d05a1331144367",
+    "url": "/static/media/business-affiliate-network.b92b0dab.svg"
+  },
+  {
+    "revision": "20371731dcf215d411b9ded30e25d32d",
+    "url": "/static/media/creative-skills.20371731.jpg"
+  },
+  {
+    "revision": "01260fc9202286db52288def453b3ceb",
+    "url": "/static/media/digital-skills.01260fc9.jpg"
+  },
+  {
+    "revision": "46a13553ed0994d91e66e829171e6e5d",
+    "url": "/static/media/edit.46a13553.svg"
+  },
+  {
+    "revision": "5ac68b606b9c88dfa692d431747909dc",
+    "url": "/static/media/fashion.5ac68b60.jpg"
+  },
+  {
+    "revision": "4de3c56f4b68fb03c633e9c85adfc2aa",
+    "url": "/static/media/graphic-design.4de3c56f.jpg"
+  },
+  {
+    "revision": "ef43159418dc700606bb438c1eb1ff54",
+    "url": "/static/media/green-search.ef431594.svg"
+  },
+  {
+    "revision": "796ee11a9a3ace77a3547ff6bc4a772d",
+    "url": "/static/media/logo.796ee11a.png"
+  },
+  {
+    "revision": "fa2e1b6f11d1880e091c7b57651250a1",
+    "url": "/static/media/magnifier.fa2e1b6f.svg"
+  },
+  {
+    "revision": "7a36008c224ec794c182028146d5fcc3",
+    "url": "/static/media/not-found.7a36008c.svg"
+  },
+  {
+    "revision": "f735ab9cabc4eeeedcfe0e78c86c920b",
+    "url": "/static/media/profile.f735ab9c.svg"
+  },
+  {
+    "revision": "6896ef4fa7f8e430b5a8365107d01b84",
+    "url": "/static/media/search.6896ef4f.svg"
+  },
+  {
+    "revision": "b9c03c16c77e6bfb071102353745d9ab",
+    "url": "/static/media/smart-worker.b9c03c16.svg"
+  },
+  {
+    "revision": "e100f6259e8405f924ce8e8b693febab",
+    "url": "/static/media/social-media.e100f625.jpg"
+  }
+]);
